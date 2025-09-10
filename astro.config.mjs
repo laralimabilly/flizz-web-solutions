a1953ec51b9fe75128b15940f274e5db2b651d24
Felipe Lara <laralimabilly@gmail.com>
2025-09-10 11:30:57 -0300

    Upgrade tailwindcss to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  integrations: [react(), tailwind()],
-  output: 'static'
+  integrations: [react()],
+  output: 'static',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
